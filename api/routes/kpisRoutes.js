6a59aefe238847f3ba57ae07f5b0cc2116896f13
kevin-it237 <ngaleuabel@gmail.com>
2022-05-16 19:17:09 +0100

    Update kpi_4 and kpi_5 to kpi_2 and kpi_2.1

--- api/routes/kpisRoutes.js
+++ api/routes/kpisRoutes.js
@@ -24,9 +24,9 @@ router.get("/:kpiId", authJwt.verifyToken, (req, res, next) => {
     //     query = 'SELECT states.country_code, organisations.short_name, organisations.YDMS_Org_id, SUM(survey_protocols.weight) as totalweight, SUM(sp_responses.weight_response) as custom_weight, SUM(sp_responses.questionnaire_response) as response, SUM((CASE WHEN sp_responses.questionnaire_response > 0 THEN survey_protocols.weight ELSE 0 END)) as weight, COUNT(*) as totalSP FROM `survey_protocols`, `organisations`, `sp_responses`, `states` WHERE survey_protocols.ydmsKpiYDMSKPIsId=? AND sp_responses.organisationYDMSOrgId=organisations.YDMS_Org_id AND states.YDMS_AU_id=organisations.YDMS_Org_id AND organisations.YDMS_Org_id=? AND sp_responses.surveyProtocolYDMSSPId=survey_protocols.YDMS_SP_id GROUP BY organisations.YDMS_Org_id'
     // }
 
-    if(kpiId === 'kpi_5') kpiId = 'kpi_4'
+    if(kpiId === 'kpi_2.1') kpiId = 'kpi_2'
     
-    if(kpiId === 'kpi_4') {
+    if(kpiId === 'kpi_2') {
         query = 'SELECT survey_protocols.questionnaire_text, states.country_code, organisations.short_name, organisations.YDMS_Org_id, sp_responses.weight_response, sp_responses.questionnaire_response FROM `survey_protocols`, `organisations`, `sp_responses`, `states` WHERE survey_protocols.ydmsKpiYDMSKPIsId=? AND sp_responses.organisationYDMSOrgId=organisations.YDMS_Org_id AND states.YDMS_AU_id=organisations.YDMS_Org_id AND sp_responses.surveyProtocolYDMSSPId=survey_protocols.YDMS_SP_id'
         // For state || just add this: organisations.YDMS_Org_id=?
         // if(role !== 'admin') {
